2a206fb246ba88d986cc01b6f33b9aefd2a1903f
ant <onekit@gmail.com>
2016-10-30 18:52:07 +0300

    tests

--- web/app_test.php
+++ web/app_test.php
@@ -3,7 +3,6 @@
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
-
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
 Debug::enable();
