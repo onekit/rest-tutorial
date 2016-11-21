bfcc6dd86a65217b77ad1f776c5d39384ea4de66
ant <onekit@gmail.com>
2016-11-21 12:30:48 +0300

    docker compose

--- web/app.php
+++ web/app.php
@@ -8,6 +8,7 @@ include_once __DIR__.'/../app/bootstrap.php.cache';
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
+
 //$kernel = new AppCache($kernel);
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
