bd96f4827de577e8f9ed57557e606f460d29697b
ant <onekit@gmail.com>
2016-11-23 20:59:42 +0300

    exp. with docker compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
 php app/console doctrine:database:create --if-not-exists
 php app/console doctrine:schema:update --force
 php app/console doctrine:fixtures:load --no-interaction
