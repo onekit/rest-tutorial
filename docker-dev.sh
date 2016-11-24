87ff2bf8676e97b6e04e7eb5fe17120b422aaafc
onekit <onekit@gmail.com>
2016-11-24 17:38:47 +0300

    docker exp.

--- docker-dev.sh
+++ docker-dev.sh
@@ -1,5 +1,4 @@
 docker-compose up -d
-docker run resttutorial_php composer install --no-interaction --optimize-autoloader
 docker run resttutorial_php php app/console doctrine:database:create --if-not-exists
 docker run resttutorial_php php app/console doctrine:schema:update --force
 docker run resttutorial_php php app/console doctrine:fixtures:load --no-interaction
