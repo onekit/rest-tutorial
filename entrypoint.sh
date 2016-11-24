199afff15bf0ea50b0df179635f378f5e59cfc0f
onekit <onekit@gmail.com>
2016-11-24 17:23:56 +0300

    change ip address

--- entrypoint.sh
+++
@@ -1,4 +0,0 @@
-php composer install --no-interaction --optimize-autoloader
-php app/console doctrine:database:create --if-not-exists
-php app/console doctrine:schema:update --force
-php app/console doctrine:fixtures:load --no-interaction
