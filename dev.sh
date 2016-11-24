34fff5d7ce15a2bb4c5e76b0b8abb42e693ba137
ant <onekit@gmail.com>
2016-11-25 00:11:05 +0300

    compose docker exp.

--- dev.sh
+++ dev.sh
@@ -1,1 +1,4 @@
 docker-compose up -d
+docker exec resttutorial_php_1 php app/console doctrine:database:create --if-not-exists
+docker exec resttutorial_php_1 php app/console doctrine:schema:update --force
+docker exec resttutorial_php_1 php app/console doctrine:fixtures:load --no-interaction
