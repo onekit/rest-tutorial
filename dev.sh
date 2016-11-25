58e6056408e421e795855e54b25a8d32fd02548c
onekit <onekit@gmail.com>
2016-11-25 15:17:00 +0300

    fix

--- dev.sh
+++ dev.sh
@@ -1,5 +1,4 @@
 docker-compose up -d
-docker restart resttutorial_db_1
 docker exec resttutorial_php_1 php app/console doctrine:database:create --if-not-exists
 docker exec resttutorial_php_1 php app/console doctrine:schema:update --force
 docker exec resttutorial_php_1 php app/console doctrine:fixtures:load --no-interaction
