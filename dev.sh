98e435b103a015371aa53868f19822e9fe0610c7
onekit <onekit@gmail.com>
2016-11-25 14:47:34 +0300

    dev env for fast demo on docker

--- dev.sh
+++ dev.sh
@@ -1,5 +1,4 @@
 docker-compose up -d
-docker exec resttutorial_db_1 echo "bind-address = 0.0.0.0" > /etc/mysql/conf.d/mysql.cnf
 docker restart resttutorial_db_1
 docker exec resttutorial_php_1 php app/console doctrine:database:create --if-not-exists
 docker exec resttutorial_php_1 php app/console doctrine:schema:update --force
