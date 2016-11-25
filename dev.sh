02a674b059c90e9ff208c28e3028beef9dfdfed7
ant <onekit@gmail.com>
2016-11-25 23:58:06 +0300

    compose docker exp.

--- dev.sh
+++ dev.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
-docker-compose down
 docker-compose up -d
+echo "Please wait while MySQL first launch..."
+sleep 5m; #Wait for MySQL run & start listen connections
+echo "Trying to create database"
 docker exec rest_php php app/console doctrine:database:create --if-not-exists
+echo "Trying to create schema"
 docker exec rest_php php app/console doctrine:schema:update --force
+echo "Trying to load fixtures"
 docker exec rest_php php app/console doctrine:fixtures:load --no-interaction
 echo "-------------------------"
 echo "Now open http://localhost"
