7edb12d0aa38e7e8ec65ab5895b5eba1d61eae2e
ant <onekit@gmail.com>
2016-11-25 13:44:28 +0300

    compose docker exp.

--- dev.sh
+++ dev.sh
@@ -1,4 +1,6 @@
 docker-compose up -d
+docker exec resttutorial_db_1 echo "bind-address = 0.0.0.0" > /etc/mysql/conf.d/mysql.cnf
+docker restart resttutorial_db_1
 docker exec resttutorial_php_1 php app/console doctrine:database:create --if-not-exists
 docker exec resttutorial_php_1 php app/console doctrine:schema:update --force
 docker exec resttutorial_php_1 php app/console doctrine:fixtures:load --no-interaction
