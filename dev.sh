8f60eafb7230f562d9f1c61bc4dd1c10eaefac4e
onekit <onekit@gmail.com>
2016-11-25 19:35:54 +0300

    fix

--- dev.sh
+++ dev.sh
@@ -1,6 +1,5 @@
 docker-compose down
 docker-compose up -d
-./wait-for-it.sh localhost:3306 --strict -t 120
 docker exec rest_php php app/console doctrine:database:create --if-not-exists
 docker exec rest_php php app/console doctrine:schema:update --force
 docker exec rest_php php app/console doctrine:fixtures:load --no-interaction
