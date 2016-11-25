38b5c8db0d8bed6b49fe3c6168ce7b67943f70e3
ant <onekit@gmail.com>
2016-11-25 18:34:39 +0300

    compose docker exp.

--- dev.sh
+++ dev.sh
@@ -1,4 +1,5 @@
 docker-compose up -d
+docker exec rest_php ./wait-for-it.sh 172.25.0.1:3306
 docker exec rest_php php app/console doctrine:database:create --if-not-exists
 docker exec rest_php php app/console doctrine:schema:update --force
 docker exec rest_php php app/console doctrine:fixtures:load --no-interaction
