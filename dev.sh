0e1e3a134f12231fa133767823df02490702f3f4
ant <onekit@gmail.com>
2016-11-25 17:04:06 +0300

    compose docker exp.

--- dev.sh
+++ dev.sh
@@ -1,7 +1,7 @@
 docker-compose up -d
-docker exec resttutorial_php_1 php app/console doctrine:database:create --if-not-exists
-docker exec resttutorial_php_1 php app/console doctrine:schema:update --force
-docker exec resttutorial_php_1 php app/console doctrine:fixtures:load --no-interaction
+docker exec rest_php php app/console doctrine:database:create --if-not-exists
+docker exec rest_php php app/console doctrine:schema:update --force
+docker exec rest_php php app/console doctrine:fixtures:load --no-interaction
 echo "-------------------------"
 echo "Now open http://localhost"
 echo "-------------------------"
