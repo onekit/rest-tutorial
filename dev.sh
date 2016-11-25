5edd0061163d9e290e50777fcb7c389d6cc8cea0
ant <onekit@gmail.com>
2016-11-25 19:18:14 +0300

    compose docker exp.

--- dev.sh
+++ dev.sh
@@ -1,9 +1,6 @@
 docker-compose down
 docker-compose up -d
-docker exec rest_php ./wait-for-it.sh 172.25.0.1:3306 --strict
-docker exec rest_php php app/console doctrine:database:create --if-not-exists
-docker exec rest_php php app/console doctrine:schema:update --force
-docker exec rest_php php app/console doctrine:fixtures:load --no-interaction
+docker exec rest_php ./wait-for-it.sh 172.25.0.1:3306 --strict -t120 -- "php app/console doctrine:database:create --if-not-exists && php app/console doctrine:schema:update --force && php app/console doctrine:fixtures:load --no-interaction"
 echo "-------------------------"
 echo "Now open http://localhost"
 echo "-------------------------"
