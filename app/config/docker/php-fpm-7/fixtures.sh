284d0b93b2f135c925cb4a15385fa2a59d306d57
ant <onekit@gmail.com>
2016-11-27 00:00:09 +0300

    compose docker exp.

--- app/config/docker/php-fpm-7/fixtures.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-chown www-data:www-data -R /app /tmp
-php app/console doctrine:database:create --if-not-exists
-php app/console doctrine:schema:update --force
-php app/console doctrine:fixtures:load --no-interaction
