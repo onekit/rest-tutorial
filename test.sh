10a8e74438c0edcea1135c51e7d23836e36f9ab4
ant <onekit@gmail.com>
2016-11-28 01:40:19 +0300

    compose docker exp.

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 php app/console cache:clear --env=test --no-debug
 php app/console doctrine:database:drop --force --env=test
 php app/console doctrine:database:create --env=test
