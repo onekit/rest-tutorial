e2b935fa8070be909ad1e7bbec4d2faa2089332f
ant <onekit@gmail.com>
2016-11-28 18:59:37 +0300

    compose docker exp.

--- app/config/docker/php-fpm-7/fixtures.sh
+++ app/config/docker/php-fpm-7/fixtures.sh
@@ -1,4 +1,3 @@
-sh -c "while ! curl --output /dev/null --silent --head --fail http://172.25.0.1:3306; do sleep 1 && echo -n .; done;"
-sh -c "cd /app && php app/console doctrine:database:create --if-not-exists"
-sh -c "cd /app && php app/console doctrine:schema:update --force"
-sh -c "cd /app && php app/console doctrine:fixtures:load --no-interaction"
+cd /app && php app/console doctrine:database:create --if-not-exists
+cd /app && php app/console doctrine:schema:update --force
+cd /app && php app/console doctrine:fixtures:load --no-interaction
