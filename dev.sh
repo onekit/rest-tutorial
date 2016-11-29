42e0ee4cbb431c5549826bce7b362a1518947d26
ant <onekit@gmail.com>
2016-11-29 17:06:59 +0300

    compose docker exp.

--- dev.sh
+++ dev.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 docker-compose up -d
 docker exec rest_php sh -c "while ! curl --output /dev/null --silent --head --fail http://172.25.0.1:3306; do sleep 1 && echo -n .; done;"
-docker exec rest_php php /app/app/console doctrine:schema:update --force
-docker exec rest_php php /app/app/console doctrine:fixtures:load --no-interaction
+docker exec rest_php sh -c "php /app/app/console doctrine:schema:update --force"
+docker exec rest_php sh -c "php /app/app/console doctrine:fixtures:load --no-interaction"
